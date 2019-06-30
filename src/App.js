a04058a5189f60a36ac329b380c088e4d610508b
umarqu <umar012894@gmail.com>
2019-06-30 12:22:08 +0100

    Hello World

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>React application</h1>
+      <p>
+        Hello World
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

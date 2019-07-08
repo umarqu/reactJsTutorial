7af60f181f8faee96928cafc8e3cbf3ec2df019d
umarqu <umar012894@gmail.com>
2019-07-08 23:23:03 +0100

    thinking react

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import './index.css';
 //import App from './6_Handling_Events/app.js'
 //import App from './8_Lists_Keys/app.js'
 //import App from './9_forms/app.js'
-import App from './10_lifting_State_Up/app.js'
+//import App from './10_lifting_State_Up/app.js'
+import ProductTable from './ThinkingReact/ProductTable.js'
 
 
 import * as serviceWorker from './serviceWorker';
@@ -36,6 +37,23 @@ const numbers = [2412,4,124,124]
 
 
 //10-.forms
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//thinking react
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(
+  <ProductTable products={PRODUCTS} />,
+  document.getElementById('container')
+);
+
 
 serviceWorker.unregister();
